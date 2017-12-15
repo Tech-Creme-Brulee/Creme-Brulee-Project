efa07de22362f45849ef61d362934556282dfbb0
Ren <rjingmembers@gmail.com>
2017-12-14 23:37:45 -0800

    created function to show or hide links based on user login status

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,40 @@
-$(document).ready(function(){
+$(document).ready(function () {
   var resultId = "97fc3192-e486-42a2-b400-6f4dd2b189dd";
+  var userSession = sessionStorage.getItem("islogged");
 
-  $("#submit-button").on("click", function(e){
+  $("#submit-button").on("click", function (e) {
     e.preventDefault();
     $("#results").html("");
     $("#results").append("<div>hello</div>");
     saveResult(resultId);
   });
 
-  function saveResult(searchResult){
-    $.post("/api/search_data",{
+  function saveResult(searchResult) {
+    $.post("/api/search_data", {
       ucpc: resultId
-    }).then(function(){
+    }).then(function () {
       displayTopResults();
     })
   }
+
+  function setLinkVisibility() {
+    userSession ? hideMemberAccessBtn() : hideMemberOnlyBtn();
+  }
+
+  function hideMemberAccessBtn() {
+    $(".member-access").hide();
+  }
+
+  function hideMemberOnlyBtn() {
+    $(".member-only").hide();
+    $(".member-access").show();
+  }
+
+
+  $(".logout").on("click", function () {
+    sessionStorage.clear();
+  })
+
+
+  setLinkVisibility();
 })
