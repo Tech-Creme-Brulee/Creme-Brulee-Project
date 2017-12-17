9233bf61063c7f633962b355cfb5370ba643b458
Hussein Fakhreddine <hfakhreddine86@gmail.com>
2017-12-17 12:15:44 -0800

    Front and Back end changes
    -Changed home and reviews page to include comment field. 
    -Removed search bar and panels from reviews page.
    -added logic to create update and delete comment (does not work at the moment).

--- public/js/index.js
+++ public/js/index.js
@@ -59,7 +59,10 @@ $(document).ready(function () {
           localStorage.setItem("ocpc", ocpc);
           saveResult(ocpc);
         });
+        // if (response.data[i].image == "https://www.cannabisreports.com/images/" + forWhat + "strains/no_image.png") {
+        // $("#results").next(b);
         $("#results").append(b);
+        // }
 
         var c = $("<p>").text(response.data[i].description);
         $(c).attr("ocpc", response.data[i].ocpc);
