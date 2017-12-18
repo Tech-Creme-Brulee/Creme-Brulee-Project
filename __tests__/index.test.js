f7e970ccb1addceb98504cecea3fe6eb819f1b82
Hussein Fakhreddine <hfakhreddine86@gmail.com>
2017-12-18 10:04:38 -0800

    testing

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,8 @@
+var $ = require("jquery");
 var multiply = require('..//public/js/index.js');
 describe("search", function () {
   it("should search properly when passed the correct search parameters", function () {
-    expect(search("")).toEqual(search(forWhat));
+    expect(search("edibles", "products", "flowers", "strains")).toEqual(search(forWhat));
   });
 
   it("should throw when not passed the correct search parameters", function () {
